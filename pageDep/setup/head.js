89ab81236e94ba0bf8738ef61ad3a5c8386a9301
Garrett Mills <garrett@glmills.gq>
2015-08-25 16:43:14 +0000

    glmills: added basic framework for dynamic cloned header and body and created std.css

---
+++ pageDep/setup/head.js
@@ -0,0 +1,15 @@
+/*
+THIS CODE IS PART OF THE GEEKFI GROUP
+PROJECT: web
+CREATION: 25 Aug 2015
+CODERS: glmills
+THIS CODE IS LICENSED UNDER THE GNU AFFERO GPL V3.0
+https://raw.githubusercontent.com/geekfi/lib/master/license.geekfi
+*/
+
+document.write('\
+    <link rel="stylesheet" type="text/css" href="/pageDep/style/index.css"/>\
+    <link rel="stylesheet" type="text/css" href="/pageDep/std.css"/>\
+    <title>geek.fi</title>\
+\
+');
